08a33080fccd0022697544cf7119c78bf0ffbc74
thammarat-ai <thammarat.th@up.ac.th>
2023-01-12 00:20:29 +0700

    ex list

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,43 @@
 import * as React from 'react'
 
-function getTitle(title){
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+
 
 function App() {
       return (
     <div>
       <div>
-        <h1>Hello {getTitle('React')}</h1>
+
+        <h1>My Hacker Stories</h1>
 
         <label htmlFor="search">Search: </label>
-        <input type="text" id="search" />
+        <input id="search" type="text" />
+
+        <hr />
+
+        <ul>
+          {list.map(function (item) {
+            return <li key={item.objectID}>{item.title}</li>;
+          })}
+        </ul>
       </div>
     </div>
   )
